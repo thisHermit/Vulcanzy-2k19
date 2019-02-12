e35d2b52f753b1cd12f2526b25f1a77e255971d8
amitsai28 <34607350+amitsai28@users.noreply.github.com>
2019-02-12 14:30:11 +0530

    Update csea.js

--- authIndividual/csea/csea.js
+++ authIndividual/csea/csea.js
@@ -38,8 +38,8 @@ function submitForm(e){
     else Gender=document.getElementById('female').value
     //save msg to firebase 
     console.log(123);
-    var VIRTUE=0,CRYPTO=0,CODESP=0,COLLOQ=0;
-    for (i =0;i<4;i++)
+    var VIRTUE=0,CRYPTO=0,CODESP=0,COLLOQ=0,WS=0;
+    for (i =0;i<5;i++)
     {
         if (events[i].checked)
         {
@@ -52,14 +52,17 @@ function submitForm(e){
                 case "CRYPTOCEON" : CRYPTO=1;
                                     break;                                    
                 case "VIRTUE" : VIRTUE=1;
-                                    break;            }
+                                    break;   
+		    case "WORKSHOP ON CRYPTOGRAPHY" : WS=1;
+			    break;
+	    }
         }
          
         
     }
     alert("Please keep the copy of downloaded form with you!!");
 
-    saveMessage(fname,lname,email,mobile,gender,College,VIRTUE,CRYPTO,CODESP,COLLOQ);
+    saveMessage(fname,lname,email,mobile,gender,College,VIRTUE,CRYPTO,CODESP,COLLOQ,WS);
     if (gender.checked)
     {
         Gender='M';
@@ -68,7 +71,7 @@ function submitForm(e){
     document.getElementById('csea').reset();
     
 
-    generatePDF(fname, email,mobile,Gender,College,checkvalue(COLLOQ),checkvalue(CRYPTO),checkvalue(VIRTUE),checkvalue(CODESP));
+    generatePDF(fname, email,mobile,Gender,College,checkvalue(COLLOQ),checkvalue(CRYPTO),checkvalue(VIRTUE),checkvalue(CODESP),checkvalue(WS));
 
 }
 function checkvalue(val)
@@ -80,7 +83,7 @@ function getInputVal(id)
 {
     return document.getElementById(id).value;
 }
-function generatePDF(name , email,mobile,gender,college,COLLOQ1,CRYPTO1,VIRTUE1,CODESP1)
+function generatePDF(name , email,mobile,gender,college,COLLOQ1,CRYPTO1,VIRTUE1,CODESP1,WS1)
 {
     var doc = new jsPDF('portrait', 'mm', 'a4');
 	var img = new Image();
@@ -102,6 +105,7 @@ function generatePDF(name , email,mobile,gender,college,COLLOQ1,CRYPTO1,VIRTUE1,
         doc.text(21,175, "CRYPTOTEON : "+CRYPTO1 );
         doc.text(21,190, "VIRTUALLY TRUE : "+VIRTUE1 );
         doc.text(21,205, "CODE SPRINT : "+CODESP1 );
+	doc.text(21,220, "WORKSHOP ON CRYPTOGRAPHY : "+WS1 );
         doc.text(21, 280, "CAMPUS AMABASSADOR SIGNATURE :");
 	//doc.addImage(img, 'PNG',42,70, 126, 160);	
 	doc.save(name+"Vulcanzy");
@@ -109,7 +113,7 @@ function generatePDF(name , email,mobile,gender,college,COLLOQ1,CRYPTO1,VIRTUE1,
 }
 //save msg to firebase
 
-function saveMessage(fname,lname,email,mobile,gender,College,VIRTUE,CRYPTO,CODESP,COLLOQ)
+function saveMessage(fname,lname,email,mobile,gender,College,VIRTUE,CRYPTO,CODESP,COLLOQ,WS)
 {
      var newMessageRef=messageRef.push();
      newMessageRef.set({
@@ -122,6 +126,7 @@ function saveMessage(fname,lname,email,mobile,gender,College,VIRTUE,CRYPTO,CODES
          CRYPTO: CRYPTO,
          CODESP: CODESP,
          COLLOQ: COLLOQ,
-         VIRTRUE:VIRTUE
+         VIRTUE:VIRTUE,
+	  WS:WS
      });
 }
