31d0cbb54a1bf104b140f2c75e4982a1de1a2268
amitsai28 <34607350+amitsai28@users.noreply.github.com>
2019-02-18 16:59:19 +0530

    UPDATED

--- authIndividual/chem/chem.js
+++ authIndividual/chem/chem.js
@@ -10,9 +10,9 @@
   firebase.initializeApp(config);
 // referance message collection
 
-var messageRef=firebase.database().ref('messages');
+ var messageRef=firebase.database().ref('messages');
 //event listener for form submit
-document.getElementById('cseaSubmit').addEventListener('submit',submitForm);
+document.getElementById('csea').addEventListener('submit',submitForm);
 //submit form 
 function submitForm(e){
 
@@ -20,33 +20,117 @@ function submitForm(e){
     
     //Get values
 
-    var name=getInputVal('name');
-    var email=getInputVal('Email');
-    var mobile=getInputVal('mob');
-    var College=getInputVal('colg');
-    var gender=document.getElementsByName('gender');
+    var fname=getInputVal('firstname');
+    var lname=getInputVal('lastname')
+    var email=getInputVal('email');
+    var mobile=getInputVal('mobile');
+    var College=getInputVal('college');
+    var gender=document.getElementById('male');
+    var Gender="";
+    var events=document.getElementsByClassName('checkbox');
+    var str=events[2].value;
+    console.log(str);
+    if (gender.checked)
+    {
+        Gender=document.getElementById('male').value;
+    }
+    else Gender=document.getElementById('female').value
     //save msg to firebase 
     console.log(123);
-    saveMessage(name,email,mobile,College,gender);
+    var VIRTUE=0,CRYPTO=0,CODESP=0,COLLOQ=0,WS=0;
+    for (i =0;i<5;i++)
+    {
+        if (events[i].checked)
+        {
+            switch(events[i].value)
+            {
+                case "LECTURES" : LECT=1;
+                                    break;
+                case "ALCHEMY" : AC=1;
+                                    break;
+                case "EX QUIZITE" : EQ=1;
+                                    break;                                    
+                case "PHEONIX" : PH=1;
+                                    break;   
+		    case "BLAST DARTS" : BD=1;
+			    break;
+              case "QUICK G" : QG=1;
+			    break;
+	    }
+        }
+         
+        
+    }
+    alert("Please keep the copy of downloaded form with you!!");
 
-    document.getElementById('cseaSubmit').reset();
+    saveMessage(fname,lname,email,mobile,gender,College,VIRTUE,CRYPTO,CODESP,COLLOQ,WS);
+    if (gender.checked)
+    {
+        Gender='M';
+    }
+    else Gender='F';
+    document.getElementById('chem').reset();
+    
+
+    generatePDF(fname, email,mobile,Gender,College,checkvalue(LECT),checkvalue(AC),checkvalue(EQ),checkvalue(PH),checkvalue(BD),checkvalue(QG));
+
 }
-
+function checkvalue(val)
+{
+    if(val==1) return "Yes";
+    else return "No";
+}
 function getInputVal(id)
 {
     return document.getElementById(id).value;
 }
+function generatePDF(name , email,mobile,gender,college,LECT1,AC1,EQ1,PH1,BD1,QG1)
+{
+    var doc = new jsPDF('portrait', 'mm', 'a4');
+	
+ 
+	
+		doc.setFontSize(22);
+		doc.setTextColor(92, 76, 76);
+		doc.text(33,25,"VULCANZY 2K19 REGISTRATION FORM")
+		doc.setFontSize(14);
+	doc.text(21, 60, "Name : "+name);
+        doc.text(21, 75, "E-Mail : "+email);
+        doc.text(21, 90, "Mobile : "+mobile);
+		doc.text(21, 105, "Gender : "+gender);
+        doc.text(21, 120, "College : "+college);
+        doc.setFontSize(18);
+	doc.text(73,140, "EVENTS REGISTERED " );
+        doc.setFontSize(14);
+	doc.text(21,160, "LECTURES : "+LECT1 );
+        doc.text(21,175, "ALCHEMY : "+AC1 );
+        doc.text(21,190, "EX QUIZITE : "+EQ1 );
+        doc.text(21,205, "PHEONIX : "+PH1 );
+	doc.text(21,220, "BLAST DARTS : "+BD1 );
+ doc.text(21,235, "QUICK G : "+QG1 );
+        doc.text(21, 280, "CAMPUS AMABASSADOR SIGNATURE :");
+		
+	doc.save(name+"Vulcanzy");
 
+}
 //save msg to firebase
 
-function saveMessage(name , email,mobile,College,gender)
+function saveMessage(fname,lname,email,mobile,gender,College,LECT,AC,EQ,PH,BD,QG)
 {
      var newMessageRef=messageRef.push();
      newMessageRef.set({
-         name: name,
+         fname: fname,
+         lname: lname,
          email:email,
          College: College,
          gender:gender,
-         mobile:mobile
+         mobile: mobile,
+         LECT: LECT,
+         AC: AC,
+         EQ: EQ,
+         PH:PH,
+	  BD:BD,
+      QG:QG
      });
 }
+
