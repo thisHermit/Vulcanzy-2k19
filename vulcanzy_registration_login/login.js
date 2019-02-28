ae5a99834fb319594e8e95f66303f04a5125b89d
Asheesh <asheeshvishvkarma123@gmail.com>
2019-02-28 19:05:29 +0530

    Add files via upload

--- vulcanzy_registration_login/login.js
+++ vulcanzy_registration_login/login.js
@@ -1,30 +1,18 @@
- // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyB3urwbUhycDY6jUTCiFloudGHBj7629M4",
-    authDomain: "fir-auth-fb5c8.firebaseapp.com",
-    databaseURL: "https://fir-auth-fb5c8.firebaseio.com",
-    projectId: "fir-auth-fb5c8",
-    storageBucket: "fir-auth-fb5c8.appspot.com",
-    messagingSenderId: "143950457061"
-  };
-  firebase.initializeApp(config);
-// referance message collection
-
-//var messageRef=firebase.database().ref('messages');
+var messageRef=firebase.database().ref('register');
 //event listener for form submit
-var username="";
-document.getElementById('login').addEventListener('submit',submitForm);
-
+var usernames=[];
+var passwords=[];
+document.getElementById('loginForm').addEventListener('submit',submitForm);
+//submit form
 function submitForm(e){
 
     e.preventDefault();
-    var username=document.getElementById("uname").value;
-    var password=document.getElementById("pwd").value;
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
     username+="";
     password+="";
-    //console.log("username:"+username+" password:"+password);
     var leadsRef = firebase.database().ref('register');
-    var flag=false;
+    var flag=true;
     leadsRef.on('value', function(snapshot) {
       var all=[];
       var all1=[];
@@ -32,32 +20,23 @@ function submitForm(e){
           var childData = childSnapshot.val();
           var str=childData.username;
           var str1=childData.password;
-        //   usernames.push(str+"");
-        //   passwords.push(str1+"");
+          usernames.push(str+"");
+          passwords.push(str1+"");
           all.push(str+"");
           all1.push(str1+"");
       });
-    //   console.log(passwords);
+      console.log(passwords);
       if(all.includes(username)&&all1.includes(password) && (all1[all.indexOf(username)]+""==password)){
-          //console.log("Exist already");
-          sessionStorage.setItem("storageName",username);
-          alert("Login Successful.. Click ok to go to Events page");
-          window.location.href = "../events.html";
-          }
-     else if(!flag){
-       window.alert("Looks like you are not registered!! \n\n Register now!!");
-       window.location.href = "./main_registration.html";
+          flag=false;
+          window.location.href = "main.html";
+      }
+      else if(all.includes(username)&&flag){
+        flag=false;
+        window.alert("incorrect username or password..");
+      }
+     else if(flag){
+       window.alert("Please goto registration page");
+       window.location.href = "register.html";
     }
-    
   });
-}
-
-function checkvalue(val)
-{
-    if(val==1) return "Yes";
-    else return "No";
-}
-function getInputVal(id)
-{
-    return document.getElementById(id).value;
 }
