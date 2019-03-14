225b4730dc5944cd286e6a944cc5d64cce1a1b38
RISHAB SINHA <37743016+srishab55@users.noreply.github.com>
2019-03-14 10:23:37 +0530

    Update login.js

--- vulcanzy_registration_login/login.js
+++ vulcanzy_registration_login/login.js
@@ -22,35 +22,48 @@ function submitForm(e){
     var password="000";
     username+="";
     password+="";
-    var leadsRef = firebase.database().ref('register');
+    var leadsRef = firebase.database().ref('register/'+username);
     var flag=true;
-    leadsRef.on('value', function(snapshot) {
-      var all=[];
-      var all1=[];
-          snapshot.forEach(function(childSnapshot) {
-          var childData = childSnapshot.val();
-          var str=childData.username;
-          var str1=childData.password;
-          usernames.push(str+"");
-          passwords.push(str1+"");
-          all.push(str+"");
-          all1.push(str1+"");
+   leadsRef.on('value',function(snapshot){
+      var user=snapshot.val();
+     console.log(123);
+     if(user!=null)
+     {
+      console.log("present");
+         sessionStorage.setItem("storageName",username);
+         window.location.href = "../index.html";
+     }
+     else 
+     {          window.alert("incorrect username or password..");
+window.location.href = "../vulcanzy_registration_login/login.html";
+     }
+//     leadsRef.on('value', function(snapshot) {
+//       var all=[];
+//       var all1=[];
+//           snapshot.forEach(function(childSnapshot) {
+//           var childData = childSnapshot.val();
+//           var str=childData.username;
+//           var str1=childData.password;
+//           usernames.push(str+"");
+//           passwords.push(str1+"");
+//           all.push(str+"");
+//           all1.push(str1+"");
       });
-      console.log(passwords);
-      if(all.includes(username)&&all1.includes(password) && (all1[all.indexOf(username)]+""==password)){
-          flag=false;
-          console.log(123); 
-          sessionStorage.setItem("storageName",username);
-          console.log(username)
-            window.location.href = "../index.html";
-      }
-      else if(all.includes(username)&&flag){
-        flag=false;
-        window.alert("incorrect username or password..");
-      }
-     else if(flag){
-       window.alert("Please goto registration page");
-       window.location.href = "../vulcanzy_registration_login/main_registration.html";
-    }
+      console.log("completed");
+//       if(all.includes(username)&&all1.includes(password) && (all1[all.indexOf(username)]+""==password)){
+//           flag=false;
+//           console.log(123); 
+//           sessionStorage.setItem("storageName",username);
+//           console.log(username)
+//             window.location.href = "../index.html";
+//       }
+//       else if(all.includes(username)&&flag){
+//         flag=false;
+//         window.alert("incorrect username or password..");
+//       }
+//      else if(flag){
+//        window.alert("Please goto registration page");
+//        window.location.href = "../vulcanzy_registration_login/main_registration.html";
+//     }
   });
 }
