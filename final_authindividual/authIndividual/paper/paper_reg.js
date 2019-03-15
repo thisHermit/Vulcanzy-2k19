201e0a1455972262b57318144f357956b9de6380
Asheesh <asheeshvishvkarma123@gmail.com>
2019-03-15 11:24:06 +0530

    Add files via upload

---
+++ final_authindividual/authIndividual/paper/paper_reg.js
@@ -0,0 +1,20 @@
+var messageRef=firebase.database().ref('paper');
+var uname=sessionStorage.getItem("storageName");
+document.getElementById("username").value=uname;
+document.getElementById('paper').addEventListener('submit',submitForm);
+function submitForm(e){
+    e.preventDefault();
+    var username=document.getElementById("username").value;
+    var sel = document.getElementById('search_categories');
+    var opt = sel.options[sel.selectedIndex];
+    writeUserData(username,opt.value);
+}
+
+function writeUserData(username,topic) {
+        firebase.database().ref('paper').child(username+"").set({
+            username: username,
+            TOPIC:topic,
+            paid: 1,
+        });
+        window.alert("registered successfully");
+}
