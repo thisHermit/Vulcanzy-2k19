aa5b7336fb85e498ee06e74c6ab5b1f148f64616
Asheesh <asheeshvishvkarma123@gmail.com>
2019-03-15 14:48:47 +0530

    Update dnd_reg.js

--- final_authindividual/authIndividual/dnd/dnd_reg.js
+++ final_authindividual/authIndividual/dnd/dnd_reg.js
@@ -38,7 +38,7 @@ function writeUserData(username,meme,boom,tell,short,dance,drama,total_fee) {
       totalfee:total_fee
   });
   window.alert("Registered Successfully");
-  window.location.href='../../../../index.html';
+  window.location.href='.../index.html';
 }
 
 function return_true(flag,value)
