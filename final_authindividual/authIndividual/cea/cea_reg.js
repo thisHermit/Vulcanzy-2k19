278ec03edec72fb1696bb1fd411087d214ec0ff0
amitsai28 <34607350+amitsai28@users.noreply.github.com>
2019-03-18 17:22:33 +0530

    modified

--- final_authindividual/authIndividual/cea/cea_reg.js
+++ final_authindividual/authIndividual/cea/cea_reg.js
@@ -7,7 +7,7 @@ var usernames=[];
 var uname=sessionStorage.getItem("storageName");
 document.getElementById("username").value=uname;
 document.getElementById('cea').addEventListener('submit',submitForm);
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxRLM9whFMC9n53gb1kcWhmYKHwe6BLVzFpm2RnMOjD1aEMnEw1/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyTvJ06EzEIyOG9EEOHGFPPT2ZQP8n-peT4bQddF9lSv27hCkx7/exec'
   const form = document.forms['cea-form']
 var total_fee=0;
 fetchData(uname);
