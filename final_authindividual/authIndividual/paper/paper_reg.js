e1fd4ab7b2cfa560f84366078039d186b2af102c
amitsai28 <34607350+amitsai28@users.noreply.github.com>
2019-03-18 19:07:09 +0530

    Update paper_reg.js

--- final_authindividual/authIndividual/paper/paper_reg.js
+++ final_authindividual/authIndividual/paper/paper_reg.js
@@ -2,6 +2,8 @@ var messageRef=firebase.database().ref('paper');
 var uname=sessionStorage.getItem("storageName");
 document.getElementById("username").value=uname;
 document.getElementById('paper').addEventListener('submit',submitForm);
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxRLM9whFMC9n53gb1kcWhmYKHwe6BLVzFpm2RnMOjD1aEMnEw1/exec'
+  const form = document.forms['paper-form']
 function submitForm(e){
     e.preventDefault();
     var username=document.getElementById("username").value;
@@ -11,6 +13,11 @@ function submitForm(e){
 }
 
 function writeUserData(username,topic) {
+    
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+    
         firebase.database().ref('paper').child(username+"").set({
             username: username,
             TOPIC:topic,
